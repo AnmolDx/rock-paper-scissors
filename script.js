653045bf70096f58ad9da43fdae2187b8eb52cde
AnmolDx <129307316+AnmolDx@users.noreply.github.com>
2026-02-05 19:15:28 +0530

    assign choices to numbers by if else

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getComputerChoise() {
    let randNum = Math.floor(Math.random()*3+1)
+
+   if(randNum === 1){
+      console.log("Rock")
+   }else if(randNum === 2){
+      console.log("Paper")
+   }else if(randNum === 3){
+      console.log("Scissors")
+   }
 }
 
